bad074f2a04fcaa5e0379a2eec9661f2dce47812
stewart-tian <stewarttian@aliyun.com>
2020-08-13 21:27:04 +0800

    eureka

--- cloud-provider-payment-8001/src/main/java/com/stewart/cloud/Payment8001Application.java
+++ cloud-provider-payment-8001/src/main/java/com/stewart/cloud/Payment8001Application.java
@@ -2,11 +2,13 @@ package com.stewart.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author stewart
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8001Application {
 
     public static void main(String[] args) {
