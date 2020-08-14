8845862773cf718ef1f43e3fe68b5139a9d6ee98
stewart-tian <stewarttian@aliyun.com>
2020-08-14 11:27:15 +0800

    eureka集群

--- cloud-eureka-server-7002/src/main/java/com/stewart/eureka/server/EurekaServer7002Application.java
+++ cloud-eureka-server-7002/src/main/java/com/stewart/eureka/server/EurekaServer7002Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer7001Application {
+public class EurekaServer7002Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServer7001Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002Application.class,args);
+    }
 
 }
