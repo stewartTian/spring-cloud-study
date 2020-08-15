82376070dcedb7472dc4194c3d35be00d763c0db
stewart-tian <stewarttian@aliyun.com>
2020-08-15 21:16:39 +0800

    zookeeper 服务调用

--- zookeeper-consumer-order-80/src/main/java/com/stewart/zookeeper/order/ZookeeperOrder80Application.java
+++ zookeeper-consumer-order-80/src/main/java/com/stewart/zookeeper/order/ZookeeperOrder80Application.java
@@ -1,16 +1,15 @@
-package com.stewart.zookeeper.payment;
-
+package com.stewart.zookeeper.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+@EnableDiscoveryClient
 @SpringBootApplication
-public class Payment8004Application {
+public class ZookeeperOrder80Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Payment8004Application.class, args);
+        SpringApplication.run(ZookeeperOrder80Application.class, args);
     }
 
 }
--- zookeeper-provider-payment-8004/src/main/java/com/stewart/zookeeper/payment/ZookeeperPayment8004Application.java
+++ zookeeper-provider-payment-8004/src/main/java/com/stewart/zookeeper/payment/ZookeeperPayment8004Application.java
@@ -1,15 +1,16 @@
-package com.stewart.zookeeper.order;
+package com.stewart.zookeeper.payment;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
 @SpringBootApplication
-public class OrderZookeeper80Application {
+public class ZookeeperPayment8004Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderZookeeper80Application.class, args);
+        SpringApplication.run(ZookeeperPayment8004Application.class, args);
     }
 
 }
