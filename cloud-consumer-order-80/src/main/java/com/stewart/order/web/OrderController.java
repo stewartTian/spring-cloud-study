7ac37c06115c19dd49c77b5160b431f7a9cb301f
stewart-tian <stewarttian@aliyun.com>
2020-08-22 18:12:49 +0800

    RestTemplate getObject和getEntity

--- cloud-consumer-order-80/src/main/java/com/stewart/order/web/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/stewart/order/web/OrderController.java
@@ -3,6 +3,8 @@ package com.stewart.order.web;
 import com.stewart.web.CommonResult;
 import com.stewart.web.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +27,17 @@ public class OrderController {
     @GetMapping("/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentEntityById(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        HttpStatus statusCode = entity.getStatusCode();
+        if (statusCode.is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(400, "查询失败");
+        }
     }
 
 }
