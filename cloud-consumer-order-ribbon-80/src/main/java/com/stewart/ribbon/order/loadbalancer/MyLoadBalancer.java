37565f645399acbc56c3199ee615e191366c185e
stewart-tian <stewarttian@aliyun.com>
2020-08-23 00:12:37 +0800

    Ribbon 自己实现轮询调用服务

---
+++ cloud-consumer-order-ribbon-80/src/main/java/com/stewart/ribbon/order/loadbalancer/MyLoadBalancer.java
@@ -0,0 +1,34 @@
+package com.stewart.ribbon.order.loadbalancer;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class MyLoadBalancer implements LoadBalancer {
+
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public final int getAndIncrement() {
+        int current;
+        int next;
+        do {
+            current = this.atomicInteger.get();
+            next = current >= 2147483647 ? 0 : current + 1;
+        } while (!this.atomicInteger.compareAndSet(current, next));
+        System.out.println("第" + next + "次访问");
+        return next;
+    }
+
+    /**
+     * 负载均衡算法
+     * rest接口第几次请求数 %  服务器集群总数量 = 实际调用服务器的位置下标，每次服务器重新启动后rest接口计数从1开始
+     */
+    @Override
+    public ServiceInstance instances(List<ServiceInstance> serviceInstances) {
+        int index = getAndIncrement() % serviceInstances.size();
+        return serviceInstances.get(index);
+    }
+}
