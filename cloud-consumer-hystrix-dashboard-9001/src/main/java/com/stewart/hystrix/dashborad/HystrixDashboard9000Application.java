2a3491c7f2876a9e3c71aa9c41a4703897a2a16b
stewart-tian <stewarttian@aliyun.com>
2020-09-02 22:21:05 +0800

    hystrix - dashborad 一直loading原因：monitor.ftlh js问题 更改方式 $(window).load(function() -> $(window).on("load",function()

---
+++ cloud-consumer-hystrix-dashboard-9001/src/main/java/com/stewart/hystrix/dashborad/HystrixDashboard9000Application.java
@@ -0,0 +1,16 @@
+package com.stewart.hystrix.dashborad;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboard9000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboard9000Application.class,args);
+    }
+
+}
