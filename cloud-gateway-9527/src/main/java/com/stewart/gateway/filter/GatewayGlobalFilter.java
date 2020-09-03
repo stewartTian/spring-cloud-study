676b2f36ed6bc4b566c6b30ac9897b03aafd09b3
stewart-tian <stewarttian@aliyun.com>
2020-09-03 21:10:11 +0800

    gateway 过滤器

---
+++ cloud-gateway-9527/src/main/java/com/stewart/gateway/filter/GatewayGlobalFilter.java
@@ -0,0 +1,37 @@
+package com.stewart.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Configuration
+public class GatewayGlobalFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("**********come in GatewayGlobalFilter:  " + LocalDate.now());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null) {
+            log.info("******用户名为null，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
