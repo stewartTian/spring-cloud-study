71febcc92587421810c78bfaab62d559d058c8c8
stewart-tian <stewarttian@aliyun.com>
2020-09-11 16:22:37 +0800

    config配置中心动态刷新

--- cloud-config-client-3311/src/main/java/com/stewart/configclient/web/ConfigClientController.java
+++ cloud-config-client-3311/src/main/java/com/stewart/configclient/web/ConfigClientController.java
@@ -8,7 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * * @RefreshScope 参数动态刷新
+ *  未接入MQ需要Post请求指定服务节点refresh刷新变更参数
  *      需要通过post请求刷新  curl -X POST “http://localhost:3311/actuator/refresh”
+ *
+ *  接入MQ：向配置中心发Post请求即可
+ *      全局更新： curl -X POST “http://localhost:3301/actuator/bus-refresh”
+ *
+ *      指定更新对应服务:   curl -X POST “http://localhost:3301/actuator/bus-refresh/cloud-config-client:3311”
+ *                      curl -X POST “http://localhost:3301/actuator/bus-refresh/cloud-config-client:3312"
+ *
  */
 @RefreshScope
 @RestController
